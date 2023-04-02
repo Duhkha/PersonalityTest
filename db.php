64048c1954929545ec76c5c38b7fa696e9d43718
Duhkha <ismar.kovacevic@stu.ibu.edu.ba>
2023-04-02 23:28:22 +0200

    first commit

---
+++ db.php
@@ -0,0 +1,58 @@
+<?php
+ 
+ 
+ require_once("rest/dao/UsersDao.class.php");
+
+ $users_dao= new UsersDAO();
+
+ $results=$users_dao->get_all();
+ print_r($results);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /*
+ $servername = "localhost";
+ $username = "root";
+ $password = "69w33d420";
+ $dbname = "personalitydb";
+
+ try {
+    // Create database connection using PDO
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+	// Prepare SQL statement
+	$stmt = $conn->prepare("SELECT * FROM users");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($result);
+
+
+
+
+
+
+
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  
+*/
+
+
+
+
+?>
---
+++ op.php
@@ -0,0 +1,42 @@
+<?php
+
+//localhost/PersonalityTest/op.php?type=add&firstname=user3&lastname=guza
+//type=delete&id=12%20OR%201=1
+//id is equal 12 or 1
+require_once("rest/dao/UsersDao.class.php");
+$users_dao= new UsersDAO();
+
+$type= $_REQUEST['type'];
+
+switch($type){
+    case 'add':
+        $firstname=$_REQUEST['firstname'];
+        $lastname=$_REQUEST['lastname'];
+        $results=$users_dao->add($firstname, $lastname);
+        print_r($results);
+        break;
+    case 'delete':
+        $id=$_REQUEST['id'];
+            $users_dao->delete($id);
+            
+        break;
+
+        case 'update':
+            $firstname=$_REQUEST['firstname'];
+            $lastname=$_REQUEST['lastname'];
+            $id=$_REQUEST['id'];
+            $users_dao->update($firstname, $lastname,$id);
+           break;
+
+            case 'get':
+                print_r('get');
+                break;
+
+    default:
+    $results=$users_dao->get_all();
+    print_r($results);
+    break;
+}
+
+
+?>
