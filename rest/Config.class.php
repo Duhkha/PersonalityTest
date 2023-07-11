c7d2e9b728376faccba799dafaa08ebbe4f7bede
zehra <zehragrbovic02@gmail.com>
2023-07-11 13:09:29 +0200

    Signup done

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 class Config{
     public static function DB_HOST(){
         return Config::get_env("DB_HOST", "localhost");
@@ -29,31 +29,39 @@ class Config{
         return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
     }
 }
+*/
 
-/*
+
 class Config{
     public static function DB_HOST(){
-        return 'localhost';
+        return Config::get_env("DB_HOST", "localhost");
     }
 
     public static function DB_USERNAME(){
-        return 'root';
+        return Config::get_env("DB_USERNAME", "root");
     }
 
     public static function DB_PASSWORD(){
-        return 'maliprinc';
+        return Config::get_env("DB_PASSWORD", "root");
     }
 //DBNAME
     public static function DB_SCHEMA(){
-        return 'personalitydb';
+        return Config::get_env("DB_SCHEMA", "personalitytest"); 
     }
 
     public static function DB_PORT(){
-        return '3307';
+        return Config::get_env("DB_PORT", "3306");
+    }
+
+    public static function JWT_SECRET(){
+        return Config::get_env("JWT_SECRET", "web");
+    }
+
+    public static function get_env($name, $default){
+        return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
     }
 }
 
-*/
 
 /*
 class Config{
